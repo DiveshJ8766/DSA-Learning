13643b45de5a24aab93e2e34b626cb5108c984ff
DiveshJ8766 <diveshvjadhav@gmail.com>
2024-06-30 14:10:12 +0530

    Logic Changed For Alphabet Palindrome Pyramid Pattern

--- 02_BasicOfProgramming-2/01_DAY1/07_AlphabetPalindromePyramid.cpp
+++ 02_BasicOfProgramming-2/01_DAY1/07_AlphabetPalindromePyramid.cpp
@@ -1,17 +1,46 @@
 #include<iostream>
 using namespace std;
 
+//* Based on row value
 void printAlphabetPalindromePyramid(int totalRows){
     //* for Rows
     for(int currentRow = 0;currentRow < totalRows;currentRow++){
         //* for Column
         for(int currentCol=0;currentCol < currentRow+1;currentCol++){
-            cout<<currentCol+1<<" ";
+            int number = currentCol+1;
+            char ch = number + 'A' - 1;
+            cout<<ch<<" ";
         }
 
         //* for Reverse from row number
         for(int reverseCol=currentRow;reverseCol >=1;reverseCol--){
-            cout<<reverseCol<<" ";
+             int number = reverseCol;
+            char ch = number + 'A' - 1;
+            cout<<ch<<" ";
+        }
+
+        cout<<endl;
+    }
+}
+
+void printAlphabetPalindromePyramid1(int totalRows){
+ //* for Rows
+    for(int currentRow = 0;currentRow < totalRows;currentRow++){
+        //* for Column
+        int currentCol;
+        for( currentCol=0;currentCol < currentRow+1;currentCol++){
+            int number = currentCol+1;
+            char ch = number + 'A' - 1;
+            cout<<ch<<" ";
+        }
+
+        currentCol-=1;
+
+        //* for Reverse from row number
+        for(;currentCol >=1;currentCol--){
+             int number = currentCol;
+            char ch = number + 'A' - 1;
+            cout<<ch<<" ";
         }
 
         cout<<endl;
@@ -23,7 +52,8 @@ int main() {
     cout<<"Enter Number Of Rows : ";
     cin>>numberOfRows;
 
-    printAlphabetPalindromePyramid(numberOfRows);
+    // printAlphabetPalindromePyramid(numberOfRows);
+    printAlphabetPalindromePyramid1(numberOfRows);
 
     return 0;
 }
