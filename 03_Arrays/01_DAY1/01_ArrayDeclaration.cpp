c0f1b5a1e1b9dce31259aed891defe84037bb05d
DiveshJ8766 <diveshvjadhav@gmail.com>
2024-07-15 23:52:53 +0530

    Array Declaration

---
+++ 03_Arrays/01_DAY1/01_ArrayDeclaration.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    //* Array Declaration
+    int arr[10];
+
+//* Address of Array
+    cout<<"Address Of Array : "<<arr<<endl;
+    cout<<"Address Of Array : "<<&arr<<endl;
+    return 0;
+}
