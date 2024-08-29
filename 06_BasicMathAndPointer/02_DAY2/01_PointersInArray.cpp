bd5a37de871a2637d0c74c7057df50adf3173d1d
Divesh Jadhav <diveshvjadhav@gmail.com>
2024-08-29 23:18:09 +0530

    Pointers In Array

---
+++ 06_BasicMathAndPointer/02_DAY2/01_PointersInArray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int *ptr = arr;
+
+    cout<<"Array Elements : ";
+    for(int i=0;i<5;i++){
+        cout<<*(ptr+i)<<" ";
+    }
+
+    cout<<endl;
+
+    cout<<"Address of Arr : "<<arr<<endl;
+    cout<<"Address of Arr : "<<&arr<<endl;
+    cout<<"Address of Arr : "<<&arr[0]<<endl;
+    cout<<"Address of Arr : "<<&0[arr]<<endl;
+
+    cout<<"Value of Arr : "<<*arr<<endl;
+    cout<<"Value of Arr : "<<*(arr + 0)<<endl;
+    cout<<"Value of Arr : "<<arr[0]<<endl;
+    cout<<"Value of Arr : "<<*(arr + 1)<<endl;
+    cout<<"Value of Arr : "<<arr[1]<<endl;
+    cout<<"Value of Arr : "<<*(arr + 2)<<endl;
+    cout<<"Value of Arr : "<<arr[2]<<endl;
+    cout<<"Value of Arr : "<<*(arr + 3)<<endl;
+    cout<<"Value of Arr : "<<arr[3]<<endl;
+    cout<<"Value of Arr : "<<*(arr + 4)<<endl;
+    cout<<"Value of Arr : "<<arr[4]<<endl;
+
+
+
+
+    return 0;
+}
