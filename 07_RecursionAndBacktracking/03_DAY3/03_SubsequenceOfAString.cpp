cf385a51cde90b006ef1ddce257cf4e986b44bc9
remiges.diveshjadhav <divesh.jadhav@remiges.tech>
2024-09-15 00:23:41 +0530

    Subsequence of a string using recursion

--- 07_RecursionAndBacktracking/03_DAY3/03_SubsequenceOfAString.cpp
+++ 07_RecursionAndBacktracking/03_DAY3/03_SubsequenceOfAString.cpp
@@ -2,13 +2,53 @@
 #include <vector>
 using namespace std;
 
-
-void printVector(vector<string> arr){
-    cout<<"Elements of Array : "<<endl;
-    for(int i =0;i < arr.size();i++){
-        cout<<arr[i]<<endl;
+void printVector(vector<string> arr)
+{
+    cout << "Elements of Array : " << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << endl;
     }
-    cout<<endl;
+    cout << endl;
+}
+
+//* using recursion and store ans in vector
+void printSubsequence2(string str, string output, int index, vector<string> &ans)
+{
+
+    //* base case
+    if (index >= str.length())
+    {
+        ans.push_back(output);
+        return;
+    }
+
+    //* Include call
+    output.push_back(str[index]);
+    printSubsequence2(str, output, index + 1, ans);
+    output.pop_back();
+
+    //* Exclude call
+    printSubsequence2(str, output, index + 1, ans);
+}
+
+//* Using Recursion
+void printSubsequence1(string str, string output, int index)
+{
+
+    //* base case
+    if (index >= str.length())
+    {
+        cout << output << " ";
+        return;
+    }
+
+    //* Exclude Call
+    printSubsequence1(str, output, index + 1);
+
+    //* Include Call
+    output.push_back(str[index]);
+    printSubsequence1(str, output, index + 1);
 }
 
 //* using for loop
@@ -22,7 +62,7 @@ void printSubsequence(string str, vector<string> &ans)
 
         for (int j = 0; j < n; j++)
         {
-            ans.push_back( ans[j] +str[i]);
+            ans.push_back(ans[j] + str[i]);
         }
     }
 }
@@ -32,10 +72,27 @@ int main()
 
     string str = "abc";
     vector<string> ans;
+    vector<string> ans1;
 
     printSubsequence(str, ans);
 
     printVector(ans);
+
+    cout << "\nSubsequence Elements : \n";
+    printSubsequence1(str, "", 0);
+    cout << "\n\n"
+         << endl;
+
+    printSubsequence2(str, "", 0, ans1);
+
+    cout << "\nSubsequence Elements : \n";
+    for (auto val : ans1)
+    {
+        cout << val << endl;
+    }
+    cout << endl;
+
+    cout << "size of ans : " << ans.size() << endl;
 
     return 0;
 }
