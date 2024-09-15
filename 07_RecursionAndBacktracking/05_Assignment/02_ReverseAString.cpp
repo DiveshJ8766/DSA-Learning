5eaa2e3cab736be612c08030b1e2ac87c3e94a57
remiges.diveshjadhav <divesh.jadhav@remiges.tech>
2024-09-16 00:33:20 +0530

    Reverse a String using recursion

--- 07_RecursionAndBacktracking/05_Assignment/02_ReverseAString.cpp
+++ 07_RecursionAndBacktracking/05_Assignment/02_ReverseAString.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
 using namespace std;
 
-
-void reverseString(string &str,int start ,int end){
+//* Using Recursion
+void reverseString(string &str, int start, int end)
+{
     //* Base case
-    if(start > end)
+    if (start > end)
         return;
-        
+
     //* ek case solve karo
-    swap(str[start],str[end]);
-    
+    swap(str[start], str[end]);
+
     //* recursive call
-    reverseString(str,start+1,end-1);
+    reverseString(str, start + 1, end - 1);
 }
 
-int main(){
-    
-    string str = "kadak";
+//* using Iterative Method
+string reverseStringIterative(string str)
+{
     int start = 0;
     int end = str.length() - 1;
-    
-    reverseString(str,start,end);
-    
-    cout<<"Reverse of a String : "<<str<<endl;
-    
+
+    while (start >= end)
+    {
+        swap(str[start], str[end]);
+        start++;
+        end--;
+    }
+
+    return str;
+}
+
+int main()
+{
+
+    string str = "divesh jadhav";
+    int start = 0;
+    int end = str.length() - 1;
+
+    //* Using Recursion
+    reverseString(str, start, end);
+    cout << "Reverse of a String : " << str << endl;
+
+    //* using Iterative Method
+    string ans = reverseStringIterative(str);
+    cout << "Reverse of a String : " << ans << endl;
+
     return 0;
 }
