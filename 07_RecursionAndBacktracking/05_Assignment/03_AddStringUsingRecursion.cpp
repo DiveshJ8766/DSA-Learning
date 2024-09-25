92593d8c1ec7e01f51ded516786f487b8477d397
remiges.diveshjadhav <divesh.jadhav@remiges.tech>
2024-09-25 23:14:02 +0530

    Add Two strings

--- 07_RecursionAndBacktracking/05_Assignment/03_AddStringUsingRecursion.cpp
+++ 07_RecursionAndBacktracking/05_Assignment/03_AddStringUsingRecursion.cpp
@@ -1,6 +1,113 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//* add two string iterative
+string addStringsIterative(string &num1, string &num2)
+{
+    //* calculate both lenghts to iterate over
+    int length1 = num1.length() - 1;
+    int length2 = num2.length() - 1;
+
+    //* carry to store carry
+    int carry = 0;
+
+    //* ans strin to store final string
+    string ans = "";
+
+    while (length1 >= 0 || length2 >= 0 || carry != 0)
+    {
+
+        //* Pointer to point elements of both the Numbers
+        //* to mark if any element is absent mark its position as Zero
+        int pointer1 = (num1[length1] >= 0 ? num1[length1] : 0) - '0';
+        int pointer2 = (num2[length2] >= 0 ? num2[length2] : 0) - '0';
+
+        cout << "Pointer1 : " << pointer1 << " Pointer2 : " << pointer2 << endl;
+
+        //* add both the numbers
+        int sum = pointer1 + pointer2 + carry;
+
+        cout << "Sum is : " << sum << endl;
+
+        //* calculate the last digit
+        int lastDigit = sum % 10;
+
+        //* update the carry
+        carry = sum / 10;
+
+        cout << "Lastdigit : " << lastDigit << " carry : " << carry << endl;
+
+        //* add the last digit to ans
+        ans.push_back(lastDigit + '0');
+
+        cout << "elements in ans : ";
+        for (auto val : ans)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+
+        length1--;
+        length2--;
+    }
+
+    //* reverse the ans
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+//* add two string using recursion
+string addStringsRecursive(string num1, int index1, string num2, int index2, string ans, int carry = 0)
+{
+
+    //* base case
+    if(index1 < 0 && index2 < 0 && carry == 0){
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    //* one case
+    int pointer1 = (num1[index1] >= 0 ? num1[index1] : 0) - '0';
+    int pointer2 = (num2[index2] >= 0 ? num2[index2] : 0) - '0';
+
+    cout << "Pointer1 : " << pointer1 << " Pointer2 : " << pointer2 << endl;
+
+    int sum = pointer1 + pointer2 + carry;
+    int lastDigit = sum % 10;
+    carry = sum / 10;
+
+    ans.push_back(lastDigit + '0');
+
+    //* recursive relation
+    return addStringsRecursive(num1, index1 - 1, num2, index2 - 1, ans, carry);
+}
+
+//* add two string using recursion pass by reference
+void addStringsRecursive1(string &num1, int index1, string& num2, int index2, string &ans, int carry = 0)
+{
+
+    //* base case
+    if(index1 < 0 && index2 < 0 && carry == 0){
+        return;
+    }
+
+    //* one case
+    int pointer1 = (num1[index1] >= 0 ? num1[index1] : 0) - '0';
+    int pointer2 = (num2[index2] >= 0 ? num2[index2] : 0) - '0';
+
+    cout << "Pointer1 : " << pointer1 << " Pointer2 : " << pointer2 << endl;
+
+    int sum = pointer1 + pointer2 + carry;
+    int lastDigit = sum % 10;
+    carry = sum / 10;
+
+    ans.push_back(lastDigit + '0');
+
+    //* recursive relation
+    addStringsRecursive1(num1, index1 - 1, num2, index2 - 1, ans, carry);
+}
+
+//* add two strings (self logic)
 string addStrings(string &num1, string &num2)
 {
     //* calculate both lengths
@@ -87,12 +194,29 @@ string addStrings(string &num1, string &num2)
 int main()
 {
 
-    string num1 = "199";
-    string num2 = "11";
+    string num1 = "7693";
+    string num2 = "4078842";
 
-    string ans = addStrings(num1, num2);
+    string ans1 = addStrings(num1, num2);
+    cout << "Addition of Two Strings : " << ans1 << endl;
 
-    cout << ans << endl;
+    string ans2 = addStringsIterative(num1, num2);
+    cout << "Addition of Two Strings : " << ans2 << endl;
+
+    cout<<endl<<endl;
+    
+    string ans3 = addStringsRecursive(num1, num1.length() - 1, num2, num2.length() - 1,"");
+    cout<<"Addition of Two Strings recursively : "<<ans3<<endl;
+
+    cout<<endl<<endl;
+
+    string ans = "";
+    addStringsRecursive1(num1, num1.length() - 1, num2, num2.length() - 1, ans);
+    
+    reverse(ans.begin(),ans.end());
+
+    cout<<"Addition of Two Strings recursively pass by reference : "<<ans<<endl;
+    cout<<endl<<endl;
 
     return 0;
 }
