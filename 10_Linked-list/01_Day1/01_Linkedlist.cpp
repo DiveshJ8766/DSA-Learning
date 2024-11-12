d7c552067d3900ae3bd52f17ffb0bbbf10235fa7
remiges.diveshjadhav <divesh.jadhav@remiges.tech>
2024-11-12 23:42:45 +0530

    Linked List Basic

--- 10_Linked-list/01_Day1/01_Linkedlist.cpp
+++ 10_Linked-list/01_Day1/01_Linkedlist.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
 int main()
 {
+    Node *head = new Node();
+    Node *one = new Node();
+    Node *two = new Node();
+    Node *three = new Node();
+
+    one->data = 5;
+    two->data = 6;
+    three->data = 8;
+
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    head->next = one;
+
+    cout << "Elements of Linked List : " << endl;
+    Node *current = head->next;
+    while (current != NULL)
+    {
+        cout << current->data << " ";
+        current = current->next;
+    }
+
+    cout << endl;
+
     return 0;
 }
